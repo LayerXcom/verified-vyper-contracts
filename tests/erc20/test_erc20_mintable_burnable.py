9366241edc46925ef5e29791c24b36e58a688ce7
yudetamago <yudetamago.orz@gmail.com>
2018-12-04 18:14:55 +0900

    Add ERC20MintableBurnable test

---
+++ tests/erc20/test_erc20_mintable_burnable.py
@@ -0,0 +1,209 @@
+import pytest
+
+ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
+
+
+@pytest.fixture
+def c(get_contract, w3, bytes_helper):
+    with open('../contracts/erc20/ERC20MintableBurnable.vy') as f:
+        code = f.read()
+    name = bytes_helper("name", 32)
+    symbol = bytes_helper("sym", 32)
+    c = get_contract(code, name, symbol, 0, 0)
+    return c
+
+
+@pytest.fixture
+def get_log_args(get_logs):
+    def get_log_args(tx_hash, c, event_name):
+        logs = get_logs(tx_hash, c, event_name)
+        assert len(logs) > 0
+        args = logs[0].args
+        return args
+    return get_log_args
+
+
+def test_initial_state(c, w3):
+    a1, a2, a3 = w3.eth.accounts[1:4]
+    # Check total supply is 0
+    assert c.totalSupply() == 0
+    # Check several account balances as 0
+    assert c.balanceOf(a1) == 0
+    assert c.balanceOf(a2) == 0
+    assert c.balanceOf(a3) == 0
+    # Check several allowances as 0
+    assert c.allowance(a1, a1) == 0
+    assert c.allowance(a1, a2) == 0
+    assert c.allowance(a1, a3) == 0
+    assert c.allowance(a2, a3) == 0
+
+
+def test_mint_and_burn(c, w3, assert_tx_failed):
+    minter, a1, a2 = w3.eth.accounts[0:3]
+
+    # Test scenario were mints 2 to a1, burns twice (check balance consistency)
+    assert c.balanceOf(a1) == 0
+    c.mint(a1, 2, transact={'from': minter})
+    assert c.balanceOf(a1) == 2
+    c.burn(2, transact={'from': a1})
+    assert c.balanceOf(a1) == 0
+    assert_tx_failed(lambda: c.burn(2, transact={'from': a1}))
+    assert c.balanceOf(a1) == 0
+    # Test scenario were mintes 0 to a2, burns (check balance consistency, false burn)
+    c.mint(a2, 0, transact={'from': minter})
+    assert c.balanceOf(a2) == 0
+    assert_tx_failed(lambda: c.burn(2, transact={'from': a2}))
+    # Check that a1 cannot burn after depleting their balance
+    assert_tx_failed(lambda: c.burn(1, transact={'from': a1}))
+    # Check that a1, a2 cannot mint
+    assert_tx_failed(lambda: c.mint(a1, 1, transact={'from': a1}))
+    assert_tx_failed(lambda: c.mint(a2, 1, transact={'from': a2}))
+    # Check that mint to ZERO_ADDRESS failed
+    assert_tx_failed(lambda: c.mint(ZERO_ADDRESS, 1, transact={'from': a1}))
+    assert_tx_failed(lambda: c.mint(
+        ZERO_ADDRESS, 1, transact={'from': minter}))
+
+
+def test_totalSupply(c, w3, assert_tx_failed):
+    # Test total supply initially, after mint, between two burns, and after failed burn
+    minter, a1 = w3.eth.accounts[0:2]
+    assert c.totalSupply() == 0
+    c.mint(a1, 2, transact={'from': minter})
+    assert c.totalSupply() == 2
+    c.burn(1, transact={'from': a1})
+    assert c.totalSupply() == 1
+    c.burn(1, transact={'from': a1})
+    assert c.totalSupply() == 0
+    assert_tx_failed(lambda: c.burn(1, transact={'from': a1}))
+    assert c.totalSupply() == 0
+    # Test that 0-valued mint can't affect supply
+    c.mint(a1, 0, transact={'from': minter})
+    assert c.totalSupply() == 0
+
+
+def test_transfer(c, w3, assert_tx_failed):
+    minter, a1, a2 = w3.eth.accounts[0:3]
+    assert_tx_failed(lambda: c.burn(1, transact={'from': a2}))
+    c.mint(a1, 2, transact={'from': minter})
+    c.burn(1, transact={'from': a1})
+    c.transfer(a2, 1, transact={'from': a1})
+    assert_tx_failed(lambda: c.burn(1, transact={'from': a1}))
+    c.burn(1, transact={'from': a2})
+    assert_tx_failed(lambda: c.burn(1, transact={'from': a2}))
+    # Ensure transfer fails with insufficient balance
+    assert_tx_failed(lambda: c.transfer(a1, 1, transact={'from': a2}))
+    # Ensure 0-transfer always succeeds
+    c.transfer(a1, 0, transact={'from': a2})
+
+
+def test_transferFrom_and_Allowance(c, w3, assert_tx_failed):
+    minter, a1, a2, a3 = w3.eth.accounts[0:4]
+    assert_tx_failed(lambda: c.burn(1, transact={'from': a2}))
+    c.mint(a1, 1, transact={'from': minter})
+    c.mint(a2, 1, transact={'from': minter})
+    c.burn(1, transact={'from': a1})
+    # This should fail; no allowance or balance (0 always succeeds)
+    assert_tx_failed(lambda: c.transferFrom(a1, a3, 1, transact={'from': a2}))
+    c.transferFrom(a1, a3, 0, transact={'from': a2})
+    # Correct call to approval should update allowance (but not for reverse pair)
+    c.approve(a2, 1, transact={'from': a1})
+    assert c.allowance(a1, a2) == 1
+    assert c.allowance(a2, a1) == 0
+    # transferFrom should succeed when allowed, fail with wrong sender
+    assert_tx_failed(lambda: c.transferFrom(a1, a3, 1, transact={'from': a3}))
+    assert c.balanceOf(a2) == 1
+    c.approve(a1, 1, transact={'from': a2})
+    c.transferFrom(a2, a3, 1, transact={'from': a1})
+    # Allowance should be correctly updated after transferFrom
+    assert c.allowance(a2, a1) == 0
+    # transferFrom with no funds should fail despite approval
+    c.approve(a1, 1, transact={'from': a2})
+    assert c.allowance(a2, a1) == 1
+    assert_tx_failed(lambda: c.transferFrom(a2, a3, 1, transact={'from': a1}))
+    # 0-approve should not change balance or allow transferFrom to change balance
+    c.mint(a2, 1, transact={'from': minter})
+    assert c.allowance(a2, a1) == 1
+    c.approve(a1, 0, transact={'from': a2})
+    assert c.allowance(a2, a1) == 0
+    c.approve(a1, 0, transact={'from': a2})
+    assert c.allowance(a2, a1) == 0
+    assert_tx_failed(lambda: c.transferFrom(a2, a3, 1, transact={'from': a1}))
+    # Test that if non-zero approval exists, 0-approval is NOT required to proceed
+    # a non-conformant implementation is described in countermeasures at
+    # https://docs.google.com/document/d/1YLPtQxZu1UAvO9cZ1O2RPXBbT0mooh4DYKjA_jp-RLM/edit#heading=h.m9fhqynw2xvt
+    # the final spec insists on NOT using this behavior
+    assert c.allowance(a2, a1) == 0
+    c.approve(a1, 1, transact={'from': a2})
+    assert c.allowance(a2, a1) == 1
+    c.approve(a1, 2, transact={'from': a2})
+    assert c.allowance(a2, a1) == 2
+    # Check that approving 0 then amount also works
+    c.approve(a1, 0, transact={'from': a2})
+    assert c.allowance(a2, a1) == 0
+    c.approve(a1, 5, transact={'from': a2})
+    assert c.allowance(a2, a1) == 5
+
+
+def test_raw_logs(c, w3, get_log_args):
+    minter, a1, a2, a3 = w3.eth.accounts[0:4]
+
+    # Check that mint appropriately emits Transfer event
+    args = get_log_args(
+        c.mint(a1, 2, transact={'from': minter}), c, 'Transfer')
+    assert args._from == ZERO_ADDRESS
+    assert args._to == a1
+    assert args._value == 2
+
+    args = get_log_args(
+        c.mint(a1, 0, transact={'from': minter}), c, 'Transfer')
+    assert args._from == ZERO_ADDRESS
+    assert args._to == a1
+    assert args._value == 0
+
+    # Check that burn appropriately emits Transfer event
+    args = get_log_args(c.burn(1, transact={'from': a1}), c, 'Transfer')
+    assert args._from == a1
+    assert args._to == ZERO_ADDRESS
+    assert args._value == 1
+
+    args = get_log_args(c.burn(0, transact={'from': a1}), c, 'Transfer')
+    assert args._from == a1
+    assert args._to == ZERO_ADDRESS
+    assert args._value == 0
+
+    # Check that transfer appropriately emits Transfer event
+    args = get_log_args(c.transfer(
+        a2, 1, transact={'from': a1}), c, 'Transfer')
+    assert args._from == a1
+    assert args._to == a2
+    assert args._value == 1
+
+    args = get_log_args(c.transfer(
+        a2, 0, transact={'from': a1}), c, 'Transfer')
+    assert args._from == a1
+    assert args._to == a2
+    assert args._value == 0
+
+    # Check that approving amount emits events
+    args = get_log_args(c.approve(a1, 1, transact={'from': a2}), c, 'Approval')
+    assert args._owner == a2
+    assert args._spender == a1
+    assert args._value == 1
+
+    args = get_log_args(c.approve(a2, 0, transact={'from': a3}), c, 'Approval')
+    assert args._owner == a3
+    assert args._spender == a2
+    assert args._value == 0
+
+    # Check that transferFrom appropriately emits Transfer event
+    args = get_log_args(c.transferFrom(
+        a2, a3, 1, transact={'from': a1}), c, 'Transfer')
+    assert args._from == a2
+    assert args._to == a3
+    assert args._value == 1
+
+    args = get_log_args(c.transferFrom(
+        a2, a3, 0, transact={'from': a1}), c, 'Transfer')
+    assert args._from == a2
+    assert args._to == a3
+    assert args._value == 0
