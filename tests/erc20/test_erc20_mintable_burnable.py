771c626a9379cc550aaba0ac41eb35ba04f8e781
中村龍矢 <nrryuya@gmail.com>
2019-01-22 10:30:29 +0900

    Fix wrong variable code → bad_code

--- tests/erc20/test_erc20_mintable_burnable.py
+++ tests/erc20/test_erc20_mintable_burnable.py
@@ -21,13 +21,13 @@ def c(get_contract, w3):
 
 
 @pytest.fixture
-def c_bad(get_contract, w3, bytes_helper):
+def c_bad(get_contract, w3):
     # Bad contract is used for overflow checks on totalSupply corrupted
     with open('../contracts/erc20/ERC20MintableBurnable.vy') as f:
         code = f.read()
     bad_code = code.replace("self.total_supply += _value", "") \
                 .replace("self.total_supply -= _value", "")
-    c = get_contract(code, *[TOKEN_NAME, TOKEN_SYMBOL, TOKEN_DECIMALS, TOKEN_INITIAL_SUPPLY])
+    c = get_contract(bad_code, *[TOKEN_NAME, TOKEN_SYMBOL, TOKEN_DECIMALS, TOKEN_INITIAL_SUPPLY])
     return c
 
 
